f82acafb8734e513cd3fe1f6fb2b89fafb9aad51
leegangeun <rkddms123456@gmail.com>
2021-12-13 16:40:36 +0900

    this is commit message

---
+++ WhyMethod.java
@@ -0,0 +1,20 @@
+public class WhyMethod {
+     
+    public static void main(String[] args) {
+         
+        // 100000000
+        printTwoTimesA();
+        // 100000000
+        printTwoTimesA();
+        // 100000000
+        printTwoTimesA();
+ 
+    }
+ 
+    public static void printTwoTimesA() {
+        System.out.println("-");
+        System.out.println("a");
+        System.out.println("a");
+    }
+ 
+}
